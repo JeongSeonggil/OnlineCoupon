47de2794ccd99b53b2149c5db715a5b0478e8c07
JeongSeonggil <dataofsg02@gmail.com>
2022-08-24 09:10:52 +0900

    Commit

---
+++ src/main/java/com/kopo/spring/SpringBootV2Application.java
@@ -0,0 +1,13 @@
+package com.kopo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kopo/spring/SpringBootV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
